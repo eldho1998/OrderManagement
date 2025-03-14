4c5dfb7eb28fafb940190274ec012fdfbf1ec4c4
Eldhose Kurian <eldhosekurian9809@gmail.com>
2025-03-14 16:13:29 +0530

    update LoginSignUp pages

---
+++ client/src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import "./HomePage.css";
+import SignUp from "../Pages/SignUp";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <SignUp />
+    </div>
+  );
+};
+
+export default HomePage;
